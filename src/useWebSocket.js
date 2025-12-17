f85b857582ffeb98d3f22581df20113135ca1aa8
Sergio <srgiothormusic@gmail.com>
2025-12-17 18:44:54 +0100

    Conectando con el servidor real de Render

--- src/useWebSocket.js
+++ src/useWebSocket.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 
 // URL de tu servidor de WebSockets
-const WS_URL = "ws://localhost:8080";
+const WS_URL = "wss://stand-destiny-backend.onrender.com";
 const ADMIN_KEY = "Srgiothor140498";
 
 const useWebSocket = () => {
